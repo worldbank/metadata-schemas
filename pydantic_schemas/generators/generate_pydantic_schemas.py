ee15f2d09b060f3b584e0884976fef309af337e2
gordonblackadder <171737385+gblackadder@users.noreply.github.com>
2024-12-06 10:22:27 -0500

    move json->python config to yaml

--- pydantic_schemas/generators/generate_pydantic_schemas.py
+++ pydantic_schemas/generators/generate_pydantic_schemas.py
@@ -1,35 +1,33 @@
-import importlib.metadata
+# import importlib.metadata
 import os
 import re
 from subprocess import run
+
+import yaml
 
 SCHEMA_DIR = "schemas"
 OUTPUT_DIR = os.path.join("pydantic_schemas")
 PYTHON_VERSION = "3.11"
 BASE_CLASS = ".utils.schema_base_model.SchemaBaseModel"
-__version__ = importlib.metadata.version("metadataschemas")
-
-INPUTS_TO_OUTPUTS = {
-    "document-schema.json": ("document_schema.py", "Document", "ScriptSchemaDraft"),
-    "geospatial-schema.json": ("geospatial_schema.py", "Geospatial", "GeospatialSchema"),
-    "image-schema.json": ("image_schema.py", "Image", "ImageDataTypeSchema"),
-    "microdata-schema.json": ("microdata_schema.py", "Microdata", "DdiSchema"),
-    "resource-schema.json": ("resource_schema.py", "Resource", "Model"),
-    "script-schema.json": ("script_schema.py", "Script", "ResearchProjectSchemaDraft"),
-    "table-schema.json": ("table_schema.py", "Table", "Model"),
-    "timeseries-db-schema.json": ("indicators_db_schema.py", "Indicators_DB", "TimeseriesDatabaseSchema"),
-    "timeseries-schema.json": ("indicator_schema.py", "Indicator", "TimeseriesSchema"),
-    "video-schema.json": ("video_schema.py", "Video", "Model"),
-}
+# __version__ = importlib.metadata.version("metadataschemas")
 
 
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
 
-for input_file, (output_file, metadata_type, schema_class_name) in INPUTS_TO_OUTPUTS.items():
-    print(f"Generating pydantic schema for {input_file}")
-    input_path = os.path.join(SCHEMA_DIR, input_file)
-    output_path = os.path.join(OUTPUT_DIR, output_file).replace("-", "_")
+with open("json_to_python_config.yaml", "r") as file:
+    data = yaml.safe_load(file)
+
+# for json_file, (python_file, metadata_type, schema_class_name) in INPUTS_TO_OUTPUTS.items():
+for section, details in data.items():
+    json_file = details["json_file"]
+    python_file = details["python_file"]
+    model_name = details["model_name"]
+    version = details["version"]
+
+    print(f"Generating pydantic schema for {json_file}")
+    input_path = os.path.join(SCHEMA_DIR, json_file)
+    output_path = os.path.join(OUTPUT_DIR, python_file).replace("-", "_")
     run(
         [
             "datamodel-codegen",
@@ -58,8 +56,8 @@ for input_file, (output_file, metadata_type, schema_class_name) in INPUTS_TO_OUT
         content = file.read()
 
     updated_content = re.sub(
-        f'class {schema_class_name}\(SchemaBaseModel\):\n(    """\n.*\n    """)',  #
-        lambda match: f"class {schema_class_name}(SchemaBaseModel):\n{match.group(1)}\n    __metadata_type__ = '{metadata_type}'\n    __metadata_type_version__='{__version__}'",
+        f'class {model_name}\(SchemaBaseModel\):\n(    """\n.*\n    """)',  #
+        lambda match: f"class {model_name}(SchemaBaseModel):\n{match.group(1)}\n    __metadata_type__ = '{section}'\n    __metadata_type_version__='{version}'",
         content,
     )
 
